55f3e9be9fce2f49192f5c0976b92c7e26191e62
Ben Armstrong <synrg@debian.org>
2025-11-02 14:14:59 -0400

    Tidy up example to make it readable

--- test_subtree_and_rank_filter.py
+++ test_subtree_and_rank_filter.py
@@ -1,17 +1,28 @@
-from pyinaturalist import ClientSession, iNatClient, make_tree, ROOT_TAXON_ID
+# Example use of make_tree and a custom include to filter
+# specific ranks within a life list subtree.
+from pyinaturalist import iNatClient, make_tree, ROOT_TAXON_ID
 
 inat = iNatClient()
-life_list = inat.observations.life_list(user_id=545640, taxon_id=3) # benarmstrong
+
+# Whole life list
+user_id = 545640 # benarmstrong
+taxon_id = 3 # birds
+life_list = inat.observations.life_list(user_id=user_id, taxon_id=taxon_id)
+
+# Subtree of life list
 root_taxon_id = 71261 # Accipitriformes
+include_ranks = ["genus"]
+
 subtree = (
     lambda t: True
     if root_taxon_id is None
     else root_taxon_id in [t.id] + [a.id for a in t.ancestors]
 )
-include = lambda t: subtree(t) and t.rank in ["genus"]
-tree = make_tree(life_list.data, include_ranks=["genus"], root_id=71261)
+include = lambda t: subtree(t) and t.rank in include_ranks
+tree = make_tree(life_list.data, include_ranks=include_ranks, root_id=root_taxon_id)
 hide_root = tree.id == ROOT_TAXON_ID
-# All genera on life list
+
+# All genera in life list subtree
 for taxon_count in tree.flatten(hide_root=hide_root):
     if include(taxon_count):
         print(taxon_count.name)
